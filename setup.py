1b6dd6b1164622d941e38fbfc7d58c10ba42a17e
Chris Drew <cdrew3@illinois.edu>
2016-11-09 21:12:09 -0600

    Updated version to 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='PyMU',
 
-    version='0.1.2',
+    version='0.2.0',
 
     description='C37.118 (PMU) data parser',
     long_description=long_description,
